5b7612fa35fc44c4dc093010a84f2ef212bf4340
Mario Savarese <mario.savarese@theguardian.com>
2021-12-24 19:03:45 +0100

    Associated function

--- chapter-5/rectangles/src/main.rs
+++ chapter-5/rectangles/src/main.rs
@@ -12,6 +12,13 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(side: u32) -> Rectangle {
+        Rectangle {
+            width: side,
+            height: side
+        }
+    }
 }
 
 fn main() {
@@ -25,10 +32,15 @@ fn main() {
         height: 20
     };
 
+    let square = Rectangle::square(10);
+
     println!("The area of rect is {}", rect1.area());
 
     match rect1.can_hold(&rect2) {
         true => println!("{:?} can hold {:?}", rect1, rect2),
         false => println!("Unfortunately, {:?} cannot hold {:?}", rect1, rect2),
     }
+
+    println!("Can rect1 hold a square? {}", rect1.can_hold(&square));
+
 }
