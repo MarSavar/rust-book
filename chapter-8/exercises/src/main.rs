27e33967b4d90397fca5d930666c7a461243bc4a
Mario Savarese <mario.savarese@theguardian.com>
2021-12-25 02:57:29 +0100

    Pig latin

--- chapter-8/exercises/src/main.rs
+++ chapter-8/exercises/src/main.rs
@@ -1,13 +1,14 @@
-// Given a list of integers, use a vector and return the mean (the average value),
-// median (when sorted, the value in the middle position),
-// and mode (the value that occurs most often; a hash map will be helpful here) of the list.
-use exercises::math::NumVector;
+use exercises::{math::NumVector, string_utils};
 
 fn main() {
     let numbers = NumVector {
         numbers: vec![1, 2, 3, 4, 5, 6, 7, 5, 4, 2, 2, 4, 3, 2, 1, 1, 9]
     };
 
+    let sentence = String::from("Hello world first apple");
+    let pig = string_utils::pig_latin(sentence);
+
+    println!("{}", pig);
     println!("{:?}", numbers.numbers);
     println!("Mean: {}", numbers.mean());
     println!("Median: {}", numbers.median());
---
+++ chapter-8/exercises/src/string_utils.rs
@@ -0,0 +1,31 @@
+// Convert strings to pig latin.
+// The first consonant of each word is moved to the end of the word and “ay” is added,
+// so “first” becomes “first-fay.”
+// Words that start with a vowel have “hay” added to the end instead
+// (“apple” becomes “apple-hay”).
+// Keep in mind the details about UTF-8 encoding!
+
+fn first_letter(string: &str) -> &str {
+    for i in 1..5 {
+        let r = string.get(0..i);
+        match r {
+            Some(x) => return x,
+            None => ()
+        }
+    }
+    &string[0..0]
+}
+
+pub fn pig_latin(initial_string: String) -> String {
+
+    let mut latin_string = String::new();
+
+    for word in initial_string.split_whitespace() {
+       let f = first_letter(&word);
+        match f {
+            "a" | "e" | "i" | "o" | "u" => latin_string += &*(word.to_owned() + "-hay "),
+            _ => latin_string += &*(word.to_owned() + "-" + f + "ay ")
+        }
+    }
+    latin_string
+}
