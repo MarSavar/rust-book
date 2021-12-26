6cd149cf35816ac54f413feac55cd7b6f790cf0a
Mario Savarese <mario.savarese@theguardian.com>
2021-12-26 22:14:20 +0100

    Better error handling

--- chapter-12/minigrep/src/main.rs
+++ chapter-12/minigrep/src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -19,12 +22,15 @@ struct Config {
     filename: String
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
 
-    Config {
-        query,
-        filename
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+       Ok(Config { query, filename })
     }
 }
