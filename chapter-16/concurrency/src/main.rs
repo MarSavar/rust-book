9e3c7ac2134426186d99f02a6d7c8c2de0685413
Mario Savarese <mario.savarese@theguardian.com>
2021-12-29 01:23:57 +0100

    More message passing

--- chapter-16/concurrency/src/main.rs
+++ chapter-16/concurrency/src/main.rs
@@ -1,13 +1,22 @@
 use std::{thread, sync::mpsc};
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hello");
-        tx.send(val).unwrap();
+        let vals = vec![String::from("Hi"),
+        String::from("from"),
+        String::from("the"),
+        String::from("thread")];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
     });
 
-    let received = rx.recv().unwrap(); // blocking operation! use try_recv for asyn
+    for received in rx {
     println!("Received \"{}\"", received);
+    }
 }
